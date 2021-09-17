bfc2d2b9086558c4dd9e5b89d8d0340b0c563026
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 05:01:14 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    age = int(input('How old are you?'))
+    
+    if 0 <= age <= 120:
+        print('Ok')
+    else:
+        print('Impossible!')
 
 if __name__ == '__main__':
     main()
